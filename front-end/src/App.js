a58b84efada54aa6ddff6ff1235e617593172c8b
cmezzac <christophermezzacappa818@yahoo.ca>
2025-02-01 18:36:06 -0500

    button and pdf viewer component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import PdfViewer from "./components/PdfViewer";
 
 export default function App() {
   return (
@@ -9,7 +10,8 @@ export default function App() {
         <div className="small-box">
           <h3>Hi I'm TAI, your learning assistant</h3>
         </div>
-        <div className="middle-box"></div>
+        <div className="middle-box">
+        </div>
         <div className="bottom-box">
           <input type="text" placeholder="Type your message..." />
         </div>
@@ -17,7 +19,14 @@ export default function App() {
 
       {/* Main Content */}
       <div className="main-content">
-        <div className="content-box"></div>
+        <div className="button-group">
+          <button className="btn">Teacher</button>
+          <button className="btn">Student</button>
+          <button className="btn btn-red">Start Recording</button>
+        </div>
+        <div className="content-box">
+          <PdfViewer />
+        </div>
         <div className="footer-box"></div>
       </div>
     </div>
---
+++ front-end/src/components/PdfViewer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function PdfViewer(){
+
+    return(
+        <p>Viewer</p>
+    );
+}
